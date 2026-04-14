37d6166997cba9e8f123c168fcae6502ad09c0ac
Brendan Coen <brendan@sonic.net>
2026-04-14 14:22:50 -0700

    move lsp config to its own lsp-config.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -14,26 +14,42 @@ return {
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>E', ':Neotree reveal expand_all<CR>', desc = 'NeoTree expand all', silent = true },
   },
   ---@module 'neo-tree'
   ---@type neotree.Config
   opts = {
+    default_component_configs = {
+      indent = {
+        with_expanders = true,
+      },
+    },
     filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-          ['P'] = {
-            'toggle_preview',
-            config = {
-              use_float = false,
-              use_image_nvim = true,
-              use_snacks_image = true,
-              title = 'Neo-tree Preview',
-            },
+      hijack_netrw_behavior = 'open_current',
+      filtered_items = {
+        visible = true,
+      },
+    },
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+        ['P'] = {
+          'toggle_preview',
+          config = {
+            use_float = false,
+            use_image_nvim = true,
+            use_snacks_image = true,
+            title = 'Neo-tree Preview',
           },
         },
-        position = 'right',
-        width = 25,
+      },
+      position = 'right',
+      width = 25,
+    },
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function() vim.cmd 'normal! zR' end,
       },
     },
   },
