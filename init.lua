301b272bfca34049e9ca3e6fda1f15d3dca34ee3
Ken Shabby <kshabby@sonic.net>
2026-07-03 15:41:08 -0700

    add clang format support

--- init.lua
+++ init.lua
@@ -111,6 +111,12 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = 'templ',
   callback = function() vim.treesitter.start() end,
 })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.cpp,*.hpp',
+  callback = function() vim.lsp.buf.format { async = false } end,
+})
+
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
